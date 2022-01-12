9325ddd75b21909c8ba32dd72abbe4f46e79ecf8
roshanram17 <51178227+roshanram17@users.noreply.github.com>
2022-01-12 15:53:20 -0500

    Add Files

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Roshan Ram on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
